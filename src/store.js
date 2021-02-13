0cf532721daf253fe142700c9b0043b99f9030a1
Thibault Brazillier <thbhcm@gmail.com>
2021-02-13 21:06:33 +0100

    feat(reducer): initialState and copy state

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { createStore } from "redux";
 
-function counterReducer(state = { value: 0 }, action) {
+const initialState = { value: 0 };
+
+function counterReducer(state = initialState, action) {
   switch (action.type) {
     case "counter/incremented":
-      return { value: state.value + 1 };
+      return { ...state, value: state.value + 1 };
     default:
       return state;
   }
